7afc330e25b1643ec413a5a8e722da20ab86cc6a
Mbukutshe <wisemanmbukutshe@gmail.com>
2017-04-20 13:57:25 +0200

    initial commit

--- app/src/main/java/shabalala/thamsanqa/tv_channel/MainActivity.java
+++ app/src/main/java/shabalala/thamsanqa/tv_channel/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // launching facebook comments activity
+                /* launching facebook comments activity*/
                 Intent intent = new Intent(MainActivity.this, FbCommentsActivity.class);
 
                 // passing the article url
